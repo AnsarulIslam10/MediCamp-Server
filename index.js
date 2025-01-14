a9a76dc6506b668ebafcd4761d124f00fab28410
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-01-14 19:56:26 +0600

    jwt token

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const port = process.env.PORT | 5000;
 
 //middleware
-app.use(cors());
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+  ],
+  credentials: true,
+  optionalSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8ggzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -20,6 +29,22 @@ const client = new MongoClient(uri, {
   },
 });
 
+// verifyToken
+const verifyToken = (req, res, next) => {
+  console.log("inside verify token", req.headers.authorization);
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -27,6 +52,19 @@ async function run() {
 
     const campCollection = client.db("mediCampDB").collection("camps");
     const userCollection = client.db("mediCampDB").collection("users");
+
+    // jwt token
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "7d",
+      });
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      }).send({ success: true})
+    });
 
     //camps related apis
     app.get("/all-camps", async (req, res) => {
@@ -52,16 +90,20 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/camp/:id', async(req, res)=>{
-      const id = req.params.id
-      const query = {_id: new ObjectId(id)}
-      const result = await campCollection.findOne(query)
-      res.send(result)
-    })
-    app.get('/popular-camps', async(req, res)=>{
-      const result = await campCollection.find().sort({participantCount: 1}).limit(6).toArray()
-      res.send(result)
-    })
+    app.get("/camp/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await campCollection.findOne(query);
+      res.send(result);
+    });
+    app.get("/popular-camps", async (req, res) => {
+      const result = await campCollection
+        .find()
+        .sort({ participantCount: 1 })
+        .limit(6)
+        .toArray();
+      res.send(result);
+    });
 
     // users related api
     app.post("/users", async (req, res) => {
@@ -75,7 +117,6 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
-
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
