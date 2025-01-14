834653675129c97f8ed15a64a04af6bb974ddd13
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-01-14 12:16:23 +0600

    popular camps

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT | 5000;
@@ -50,6 +50,17 @@ async function run() {
       const result = await campCollection.find(query).sort(sort).toArray();
       res.send(result);
     });
+
+    app.get('/camp/:id', async(req, res)=>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await campCollection.findOne(query)
+      res.send(result)
+    })
+    app.get('/popular-camps', async(req, res)=>{
+      const result = await campCollection.find().sort({participantCount: 1}).limit(6).toArray()
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
