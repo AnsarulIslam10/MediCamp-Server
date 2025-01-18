b03b6851f54953963d347256afeb2204a7c3da20
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-01-18 13:39:24 +0600

    server deployed

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+//middleware
 app.use(cors());
 app.use(express.json());
 
