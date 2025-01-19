37f1c1183cf0f4188a559d5e28617257d0509774
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-01-19 22:19:48 +0600

    done

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const campCollection = client.db("mediCampDB").collection("camps");
     const userCollection = client.db("mediCampDB").collection("users");
@@ -448,7 +448,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     // console.log(
     //   "Pinged your deployment. You successfully connected to MongoDB!"
     // );
